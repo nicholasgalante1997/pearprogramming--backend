fe56a8ca2ab2161c8febbd553ba97bd5c4c70a4c
nicholasgalante1997 <nicholasgalante1997@gmail.com>
2020-08-14 19:45:37 -0400

    redux refactor + friends join

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,14 @@ class User < ApplicationRecord
     has_many :challenges, through: :my_challenges 
     has_many :notes, through: :my_challenges 
 
+    # FOLLOWING 
+    has_many :followed_users, foreign_key: :follower_id, class_name: 'Follow'
+    has_many :followees, through: :followed_users
+    
+    # FOLLOWERS 
+    has_many :following_users, foreign_key: :followee_id, class_name: 'Follow'
+    has_many :followers, through: :following_users
+
     #Auth
     validates :username, uniqueness: true
     has_secure_password 
@@ -13,3 +21,12 @@ class User < ApplicationRecord
         return "#{self.username} says hello!"
     end 
 end
+
+
+# MEDIUM DESC 
+    # The first line of this has many through relationship is the one that’s likely to cause a little confusion so let’s break it down. 
+    # We’re saying that a user can have many followed_users, meaning that they can follow as many people as they want. 
+    # We’re going to use the foreign key follower_id to represent these followed_users and this can be found in the Follow class.
+    # Go through it a time or two to make sure you get what’s going on, because it’s clear sailing after this.
+    # The second line of code is exactly what you’d expect to see in any has_many_through relationship. 
+    # We’re saying that we have many followees through the followed_users that we’ve defined above
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_12_180920) do
+ActiveRecord::Schema.define(version: 2020_08_14_203026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 2020_08_12_180920) do
     t.integer "user_id"
     t.integer "post_id"
     t.string "text_content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followee_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
